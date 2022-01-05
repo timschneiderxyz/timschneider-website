aeb9719b3116cac378d937447a430c15d6a76470
Tim Schneider <timschneider.xyz@gmail.com>
2022-01-05 22:29:12 +0100

    updated tailwind config

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,11 @@ import React from 'react';
 const Home = () => {
   return (
     <main className='p-home'>
-      <div className='container mx-auto'>Hello World!</div>
+      <div className='container mx-auto'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nulla quos dolores libero, alias
+        minus magnam rerum eligendi, molestias adipisci illo officiis esse quidem. Voluptas sed
+        voluptates optio ipsam nemo.
+      </div>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,31 @@
     ========================================================================  */
 
 module.exports = {
-  mode: 'jit',
-  purge: {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    layers: ['components', 'utilities'],
-    options: {
-      whitelist: []
-    }
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  corePlugins: {
+    preflight: false
   },
-  darkMode: false,
+  plugins: [],
   theme: {
-    // Replace
+    fontFamily: {
+      sans: ['Rubik', 'Arial', 'sans-serif'],
+      serif: ['Georgia', 'Times New Roman', 'serif'],
+      mono: ['Consolas', 'Monaco', 'Ubuntu Mono', 'monospace']
+    },
     screens: {
       sm: '576px',
       md: '768px',
       lg: '992px',
       xl: '1200px'
     },
-    // Extend
     extend: {
-      colors: {}
+      colors: {
+        'tim-white': '#f8f8f8',
+        'tim-gray': '#8e9ba8',
+        'tim-black-1': '#060606',
+        'tim-black-2': '#0f1012',
+        'tim-primary': '#e67066'
+      }
     }
-  },
-  variants: {},
-  plugins: []
+  }
 };
