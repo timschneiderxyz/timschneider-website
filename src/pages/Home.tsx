7603c76fd5a502ab195ef4a914c6935e4c9426a5
Tim Schneider <timschneider.xyz@gmail.com>
2022-08-07 21:20:41 +0200

    init about and cta section + updated redirects/packages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,7 @@ import useGitHub from '@hooks/useGitHub';
 
 // Components
 import Section from '@components/Section';
+// import List from '@components/List';
 import { RepoCard, RepoCardSkeleton } from '@components/RepoCard';
 
 const Home = () => {
@@ -24,6 +25,41 @@ const Home = () => {
         </div>
       </Section>
 
+      {/* <Section id='about'>
+        <div className='grid lg:grid-cols-[1fr_2fr] gap-10 lg:gap-20'>
+          <div>
+            <h2>About me</h2>
+          </div>
+          <div>
+            <List
+              title='Tools & Technologies'
+              items={[
+                'Git',
+                'Docker',
+                'Node.js',
+                'Webpack',
+                'React',
+                'WordPress',
+                'Tailwind CSS',
+                'Databases (SQL)',
+                'APIs (REST, GraphQL)'
+              ]}
+            />
+            <List
+              title='Languages'
+              items={[
+                'HTML5',
+                'CSS3 / SCSS',
+                'JavaScript / TypeScript',
+                'PHP',
+                'Bash',
+                'PowerShell'
+              ]}
+            />
+          </div>
+        </div>
+      </Section> */}
+
       <Section id='repos'>
         <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-4 lg:gap-y-8 max-w-5xl mx-auto'>
           {loadingRepos
@@ -34,6 +70,12 @@ const Home = () => {
           <h3 className='text-center'>An error occurred while loading repositories.</h3>
         ) : null}
       </Section>
+
+      {/* <Section id='cta'>
+        <div className='flex flex-col justify-center items-center h-[420px] md:h-[600px] p-6 bg-tim-coal rounded-xl text-center'>
+          <h2>Contact me</h2>
+        </div>
+      </Section> */}
     </main>
   );
 };
