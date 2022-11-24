5230de145fa1c18095a53738831e1cb04c9e61ae
Tim Schneider <timschneider.xyz@gmail.com>
2022-11-25 00:32:57 +0100

    updated breakpoints

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,8 +13,9 @@ const Header = () => {
   return (
     <header>
       <div className='container flex justify-between py-10'>
-        <Link to='/' title='Link to Home' aria-label='Link to Home'>
-          <Logo className='w-8 h-8 fill-tim-primary' />
+        <Link className='flex items-center' to='/' title='Link to Home' aria-label='Link to Home'>
+          <Logo className='w-8 h-8 mr-3 fill-tim-primary' />
+          <strong className='text-white'>Tim Schneider</strong>
         </Link>
       </div>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,20 @@ module.exports = {
   },
   plugins: [],
   theme: {
+    // Replace the default config.
     fontFamily: {
       sans: ['Rubik', ...defaultTheme.fontFamily.sans],
       serif: [...defaultTheme.fontFamily.serif],
       mono: [...defaultTheme.fontFamily.mono]
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1600px'
+    },
+    // Extend the default config.
     extend: {
       colors: {
         'tim-black': '#060606',
