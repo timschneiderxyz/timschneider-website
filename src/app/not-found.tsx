449ea3147553dbf544faf76bf011442040cacc5a
Tim Schneider <timschneider.xyz@gmail.com>
2023-10-26 10:36:35 +0200

    updated not-found/headings styling + packages

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,16 +12,18 @@ import schmuserkadser from '@/images/schmuserkadser.png';
 const NotFound = () => {
   return (
     <main>
-      <Section id='four-oh-four'>
-        <h1 className='text-center'>Page not found</h1>
-        <figure className='max-w-xs mt-16 mx-auto'>
-          <Image
-            className='w-full h-full'
-            src={schmuserkadser}
-            alt='A picture of Schmuserkadser.'
-            placeholder='blur'
-          />
-        </figure>
+      <Section id='four-oh-four' className='flex justify-center items-center w-full min-h-screen'>
+        <div className='container'>
+          <h1 className='text-center'>Page not found</h1>
+          <figure className='max-w-xs mt-16 mx-auto'>
+            <Image
+              className='w-full h-full'
+              src={schmuserkadser}
+              alt='A picture of Schmuserkadser.'
+              placeholder='blur'
+            />
+          </figure>
+        </div>
       </Section>
     </main>
   );
